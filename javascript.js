b5c02251cc06d17ba01aef5bfe7df57a97b01561
ophanim777 <rikyjo49@gmail.com>
2025-08-25 09:35:02 +0700

    add logic

--- javascript.js
+++ javascript.js
@@ -34,4 +34,9 @@ function playRound(humanChoice, computerChoice) {
     function playGame() {
       humanScore = 0;
       computerScore = 0;
+      for (let i = 0; i < 3; i++) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  }
     }
