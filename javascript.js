335a64e69a91149fd32ab1560ac2ec153f8efb44
ophanim777 <rikyjo49@gmail.com>
2025-08-25 09:24:08 +0700

    add new variables

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+let humanScore= 0;
+let computerScore = 0;
 function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
   let computerChoice = choices.at(Math.floor(Math.random() * choices.length));
