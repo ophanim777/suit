93e665a285ce92e17c7eeba3be5eff74096f2141
ophanim777 <rikyjo49@gmail.com>
2025-10-26 16:50:21 +0700

    delete prompt humanchoice

--- javascript.js
+++ javascript.js
@@ -4,13 +4,7 @@ function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
   let computerChoice = choices.at(Math.floor(Math.random() * choices.length));
   return computerChoice;}
-function getHumanChoice() {
-  let choices = prompt("Enter rock, paper, or scissors:").toLowerCase();
-  while (!["rock", "paper", "scissors"].includes(choices)) {
-    choices = prompt("Invalid choice. Please enter rock, paper, or scissors:").toLowerCase();
-  }
-  return choices;
-}
+  
 function playRound(humanChoice, computerChoice) {
   humanChoice = humanChoice.toLowerCase();
   computerChoice = computerChoice.toLowerCase();
